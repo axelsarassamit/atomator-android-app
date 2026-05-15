86840389dcd045d2966dcf77c3b8b8de40ddd248
Axel Sarassamit <axel.sarassamit@gmail.com>
2026-05-15 20:10:36 +0700

    Trigger clean build with latest workflow

--- lib/version.dart
+++ lib/version.dart
@@ -1,1 +1,3 @@
-const String appVersion = "1.1.0";\nconst String appBuild = "2";\nconst String basedOn = "Atomator CLI v.02.09.00";\n
+const String appVersion = '1.1.0';
+const String appBuild = '3';
+const String basedOn = 'Atomator CLI v.02.09.00';
