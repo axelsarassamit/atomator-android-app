c0357615840255d7cfe83233d5ae66466739dd7b
Axel Sarassamit <axel.sarassamit@gmail.com>
2026-05-15 20:15:53 +0700

    Add Android patch script

---
+++ scripts/patch_android.py
@@ -0,0 +1,24 @@
+import sys
+
+# Fix app name
+with open('android/app/src/main/AndroidManifest.xml', 'r') as f:
+    c = f.read()
+c = c.replace('android:label="atomator_app"', 'android:label="Atomator"')
+with open('android/app/src/main/AndroidManifest.xml', 'w') as f:
+    f.write(c)
+
+# Fix NDK
+with open('android/app/build.gradle.kts', 'r') as f:
+    c = f.read()
+c = c.replace('android {', 'android {\n        ndkVersion = "27.0.12077973"', 1)
+with open('android/app/build.gradle.kts', 'w') as f:
+    f.write(c)
+
+# Add Google Maven mirror
+with open('android/settings.gradle.kts', 'r') as f:
+    c = f.read()
+c = c.replace('gradlePluginPortal()', 'google()\n                gradlePluginPortal()')
+with open('android/settings.gradle.kts', 'w') as f:
+    f.write(c)
+
+print('Android config patched successfully')
