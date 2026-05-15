1647060722f0cc8a60e822b88ca7941a37a2115e
Axel Sarassamit <axel.sarassamit@gmail.com>
2026-05-15 23:25:13 +0700

    v1.2.2: Bump version

--- lib/version.dart
+++ lib/version.dart
@@ -1,3 +1,3 @@
-const String appVersion = '1.2.1';
-const String appBuild = '12';
+const String appVersion = '1.2.2';
+const String appBuild = '13';
 const String basedOn = 'Atomator CLI v.02.09.00';
