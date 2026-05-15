fce748c7994203a9034e8051908a52d41e7dab10
Axel Sarassamit <axel.sarassamit@gmail.com>
2026-05-15 23:10:21 +0700

    v1.2.0: In-app updates

--- lib/version.dart
+++ lib/version.dart
@@ -1,3 +1,3 @@
-const String appVersion = '1.1.7';
-const String appBuild = '10';
+const String appVersion = '1.2.0';
+const String appBuild = '11';
 const String basedOn = 'Atomator CLI v.02.09.00';
