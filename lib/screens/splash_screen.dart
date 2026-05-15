b55e988a47c3b82ca439d8d7fccb059db150a36e
axelsarassamit <axel.sarassamit@gmail.com>
2026-05-15 19:11:05 +0700

    Add splash screen with logo fade-in

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  final Widget nextScreen;
+  const SplashScreen({super.key, required this.nextScreen});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _fadeIn;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this, duration: const Duration(milliseconds: 1500));
+    _fadeIn = Tween<double>(begin: 0.0, end: 1.0).animate(CurvedAnimation(parent: _controller, curve: Curves.easeIn));
+    _controller.forward();
+    Future.delayed(const Duration(seconds: 3), () {
+      if (mounted) {
+        Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (_) => widget.nextScreen));
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF0D1117),
+      body: Center(
+        child: FadeTransition(
+          opacity: _fadeIn,
+          child: Padding(
+            padding: const EdgeInsets.all(48),
+            child: Image.asset('assets/images/splash_logo.png', fit: BoxFit.contain),
+          ),
+        ),
+      ),
+    );
+  }
+}
