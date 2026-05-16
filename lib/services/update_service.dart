a90940e796787175b952ac8bd1b5b0b8e5bbc97e
Axel Sarassamit <axel.sarassamit@gmail.com>
2026-05-16 17:33:03 +0700

    v1.4.4: Update currentVersion

--- lib/services/update_service.dart
+++ lib/services/update_service.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 import 'package:open_filex/open_filex.dart';
 
 class UpdateService {
-  static const String currentVersion = '1.4.3';
+  static const String currentVersion = '1.4.4';
   static const String _repo = 'axelsarassamit/atomator-android-app';
 
   static Future<Map<String, dynamic>?> checkForUpdate() async {
