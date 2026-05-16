47718f2d822cf021490a4fb46634344d86f88ef5
Axel Sarassamit <axel.sarassamit@gmail.com>
2026-05-16 12:35:50 +0700

    v1.3.0: Bump version - SSH terminal

--- lib/version.dart
+++ lib/version.dart
@@ -1,3 +1,3 @@
-const String appVersion = '1.2.25';
-const String appBuild = '36';
+const String appVersion = '1.3.0';
+const String appBuild = '37';
 const String basedOn = 'Atomator CLI v.02.09.00';
