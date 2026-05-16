f0c6d51a4f9d5a8a8902ed2ef8ead51a691c852c
Axel Sarassamit <axel.sarassamit@gmail.com>
2026-05-16 12:35:49 +0700

    v1.3.0: Add Terminal tab to navigation

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -4,6 +4,7 @@ import 'hosts_screen.dart';
 import 'actions_screen.dart';
 import 'tools_screen.dart';
 import 'settings_screen.dart';
+import 'terminal_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -13,7 +14,7 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   int _index = 0;
-  final _screens = const [DashboardScreen(), HostsScreen(), ActionsScreen(), ToolsScreen(), SettingsScreen()];
+  final _screens = const [DashboardScreen(), HostsScreen(), ActionsScreen(), TerminalScreen(), ToolsScreen(), SettingsScreen()];
   @override
   Widget build(BuildContext context) => Scaffold(
     body: _screens[_index],
@@ -24,6 +25,7 @@ class _HomeScreenState extends State<HomeScreen> {
         NavigationDestination(icon: Icon(Icons.dashboard), label: 'Dashboard'),
         NavigationDestination(icon: Icon(Icons.computer), label: 'Hosts'),
         NavigationDestination(icon: Icon(Icons.play_arrow), label: 'Actions'),
+        NavigationDestination(icon: Icon(Icons.terminal), label: 'Terminal'),
         NavigationDestination(icon: Icon(Icons.build), label: 'Tools'),
         NavigationDestination(icon: Icon(Icons.settings), label: 'Settings'),
       ],
